11173c48b8e6bb08674ba76b3a1a6be76c0a8d4f
Lindsay Elliott <lindsayelliott4@gmail.com>
2026-01-04 18:32:18 -0500

    middleware tests work

--- api/account.go
+++ api/account.go
@@ -67,13 +67,13 @@ func (server *Server) getAccount(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, account)
 }
 
-type listAccountRequest struct {
+type listAccountsRequest struct {
 	PageID   int32 `form:"page_id" binding:"required,min=1"`
 	PageSize int32 `form:"page_size" binding:"required,min=5,max=10"`
 }
 
-func (server *Server) listAccount(ctx *gin.Context) {
-	var req listAccountRequest
+func (server *Server) listAccounts(ctx *gin.Context) {
+	var req listAccountsRequest
 	if err := ctx.ShouldBindQuery(&req); err != nil {
 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
 		return
